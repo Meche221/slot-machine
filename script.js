171a1081f98f4042ccb746e87140eb42279c5f9f
Meche221 <binaymeche22@gmail.com>
2025-12-27 10:32:45 +0545

    adding acondition so that bet is valid and is collected inthe fucntion

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-//  1. Deposit some money
-//  2. Determine number of lines to bet
-//  3. Collect a bet amount
+//  1. Deposit some money   ✅
+//  2. Determine number of lines to bet   ✅ 
+//  3. Collect a bet amount    ✅
 //  4. Spin the slot machine
 //  5. check if the user won
 //  6. give the user their winnings
@@ -21,4 +21,34 @@ const deposit = () => {
   }
 };
 
-const depositAmount = deposit();
+const getNumberOfLines = () => {
+    while (true) {
+        const lines = prompt("Enter the number of Lines to bet on (1-3): ");
+        const numberOfLines = Number(lines);
+
+        if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3){
+            console.log("Enter a valid Lines you want to bet, Try Again")
+        } else {
+            return numberOfLines;
+        }
+    }
+}
+
+const getBet = (balance, lines) => {
+    while (true) {
+        const bet = prompt("Enter the total bet per line: ");
+        const numberBet = Number(bet);
+
+        if(isNaN(numberBet) || numberBet > (balance / lines) || numberBet <= 0) {
+            console.log("Invalid amount, Try Again")
+        } else {
+            return numberBet;
+        }
+    }
+}
+
+let balance = deposit();
+const numberOfLines = getNumberOfLines();
+const bet = getBet(balance, numberOfLines);
+
+console.log(bet);
